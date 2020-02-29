09af0a9fa9359d05d25322641128fc8a736aeda4
Delphinite <33256663+Delphinite@users.noreply.github.com>
2020-02-29 16:27:19 -0500

    Add File header and some basic functionality

--- dressmeserver.js
+++ dressmeserver.js
@@ -1,2 +1,31 @@
+/*
+  Author(s): Cameron Fritz,
+             Justin Yin,
+             Jacy Yu,
+             Yi Yang
+  
+  Date Created: 2/29/2020
+  Last Updated: 2/29/2020
+  
+  File: dressmeserver.js
+  Purpose: Script for a Node.js server for the website dressme
+*/
 let express = require("express");
+let app = express();
 
+app.use(express.static('public'));
+
+// Sets some necessary headers to allow requests to the server
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+// Test endpoint
+app.get("/test", function(req, res){
+  console.log("Hello");
+  res.send("<h1>Hello</h1>");
+});
+
+app.listen(8080);
