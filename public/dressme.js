2108215f4f8345376d9593470fcdf4b10c33b03a
Jacy Yu <jyu061313@gmail.com>
2020-03-03 16:40:17 -0500

    event listeners

---
+++ public/dressme.js
@@ -0,0 +1,12 @@
+document.getElementById("all").addEventListener("click", all);
+document.getElementById("shirts").addEventListener("click", shirts);
+document.getElementById("pants").addEventListener("click", pants);
+document.getElementById("outerwear").addEventListener("click", outerwear);
+document.getElementById("accessories").addEventListener("click", accessories);
+document.getElementById("footwear").addEventListener("click", footwear);
+document.getElementById("hats").addEventListener("click", hats)
+document.getElementById("other").addEventListener("click", other)
+
+function all(){
+
+}
