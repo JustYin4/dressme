3ace0d04ae5a414b2a07ca6f8e7e0db162b15b86
Delphinite <33256663+Delphinite@users.noreply.github.com>
2020-03-05 12:52:50 -0500

    Update the test endpoint

--- dressmeserver.js
+++ dressmeserver.js
@@ -5,7 +5,7 @@
              Yi Yang
   
   Date Created: 2/29/2020
-  Last Updated: 2/29/2020
+  Last Updated: 3/5/2020
   
   File: dressmeserver.js
   Purpose: Script for a Node.js server for the website dressme
@@ -24,8 +24,8 @@ app.use(function(req, res, next) {
 
 // Test endpoint
 app.get("/test", function(req, res){
-  console.log("Hello");
-  res.send("<h1>Hello</h1>");
+  console.log(req.query.clothType);
+  res.send("<h1>" + String(req.query.clothType) + "</h1>");
 });
 
 app.listen(8080);
