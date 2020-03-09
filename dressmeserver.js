0091906d5c187e118c8f219b90946dd7f2a576c8
Delphinite <33256663+Delphinite@users.noreply.github.com>
2020-03-09 14:12:45 -0400

    Login functionality that requires more work
    Needs connection to the database in order to progress

--- dressmeserver.js
+++ dressmeserver.js
@@ -11,15 +11,60 @@
   Purpose: Script for a Node.js server for the website dressme
 */
 let express = require("express");
+let session = require("client-sessions");
+const bp = require("body-parser");
+
 let app = express();
 
 app.use(express.static('public'));
+app.use(bp.urlencoded({extended:false}));
+app.use(bp.json());
 
 // Sets some necessary headers to allow requests to the server
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
+});
+
+// Creates a session
+app.use(session({
+	cookieName: 'session',
+	secret: 'asdfasdf23423',
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000,
+}));
+
+// Requirement for the login module
+const login = require("./login");
+
+// Login Endpoint
+app.post("/login", function(req, res){
+	login.login(conn, req.body.user, req.body.pass, function(val){
+		if (val <= 0){
+			req.session.msg = "Invalid login";
+			res.redirect("./");
+		} else {
+			req.session.user = req.body.user;
+			res.redirect("./welcome");
+		}
+	});
+});
+
+// Logout endpoint - Not used yet
+app.get("/logout", function(req, res){
+	req.session.reset();
+	req.session.msg = "Goodbye";
+	return res.redirect('/');
+});
+
+// Sends a welcome message on successful login
+app.get("/welcome", function(req, res){
+	if(!req.session.user){
+		req.session.msg = 'Access Denied';
+		return res.redirect('/');
+	}
+	res.send("Welcome" + req.session.user);
 });
 
 // Test endpoint
