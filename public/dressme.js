48afa941f4dd378d27f49afea7cee36e9b739e86
yy497 <yy497@drexel.edu>
2020-03-09 19:33:07 -0400

    database

--- public/dressme.js
+++ public/dressme.js
@@ -12,70 +12,157 @@
 */
 
 // Makes sure that event listeners are added after the web page is loaded
-window.onload = function(){
-	document.getElementById("all").addEventListener("click", allButton);
-	document.getElementById("shirts").addEventListener("click", shirts);
-	document.getElementById("pants").addEventListener("click", pants);
-	document.getElementById("outerwear").addEventListener("click", outerwear);
-	document.getElementById("accessories").addEventListener("click", accessories);
-	document.getElementById("footwear").addEventListener("click", footwear);
-	document.getElementById("hats").addEventListener("click", hats)
-	document.getElementById("others").addEventListener("click", others)
-	document.getElementById("loginForm").addEventListener("click", login);
+window.onload = function() {
+    document.getElementById("all").addEventListener("click", allButton);
+    document.getElementById("shirts").addEventListener("click", shirts);
+    document.getElementById("pants").addEventListener("click", pants);
+    document.getElementById("outerwear").addEventListener("click", outerwear);
+    document.getElementById("accessories").addEventListener("click", accessories);
+    document.getElementById("footwear").addEventListener("click", footwear);
+    document.getElementById("hats").addEventListener("click", hats)
+    document.getElementById("others").addEventListener("click", others)
+    document.getElementById("loginForm").addEventListener("click", login);
 }
 
 // Event listeners that call a request chain for getting the proper clothing
-function allButton(){
-	sendGet("all");
+function allButton() {
+    sendGet("all");
+    retrieve("all").then(function(result) {
+        document.getElementById("picture").src = result[1]
+    }).catch(function(error) {
+        console.log(error)
+    })
 }
 
-function shirts(){
-	sendGet("shirts");
+function shirts() {
+    sendGet("shirts");
 }
 
-function pants(){
-	sendGet("pants");
+function pants() {
+    sendGet("pants");
 }
 
-function outerwear(){
-	sendGet("outerwear");
+function outerwear() {
+    sendGet("outerwear");
 }
 
-function accessories(){
-	sendGet("accessories");
+function accessories() {
+    sendGet("accessories");
 }
 
-function footwear(){
-	sendGet("footwear");
+function footwear() {
+    sendGet("footwear");
 }
 
-function hats(){
-	sendGet("hats");
+function hats() {
+    sendGet("hats");
 }
 
-function others(){
-	sendGet("others");
+function others() {
+    sendGet("others");
 }
 
 // Send request to server to login
-function login(){
-	let xhr = new XMLHttpRequest();
-	xhr.addEventListener("load", updateShow);
-	xhr.open("POST", "http://localhost:8080/login");
-	xhr.send();
+function login() {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", updateShow);
+    xhr.open("POST", "http://localhost:8080/login");
+    xhr.send();
 }
 
 // Sends the request to the server
-function sendGet(clothType){
-	let xhr = new XMLHttpRequest();
-	xhr.addEventListener("load", updateShow);
-	xhr.open("GET", "http://localhost:8080/test?clothType=" + clothType);
-	xhr.send();
+function sendGet(clothType) {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", updateShow);
+    xhr.open("GET", "http://localhost:8080/test?clothType=" + clothType);
+    xhr.send();
 }
 
 // Updates the show with the response
 // TODO: Update with pictures
-function updateShow(){
-	let display = document.getElementById("show");
-	display.innerHTML = this.response;
+function updateShow() {
+    let display = document.getElementById("show");
+    display.innerHTML = this.response;
+}
+
+function upload() {
+    var selected = document.getElementById("type");
+    var selectedVal = selected.options[selected.selectedIndex].value
+    var image = document.getElementById("image").files[0];
+    var imageName = image.name;
+    var rawImageName = imageName.replace(/\..+$/, '')
+    var storageRef = firebase.storage().ref("images/" + selectedVal + "/" + imageName);
+
+    // Checks if image already exists
+    if (imageName in firebase.storage().ref("images/all/")) {
+        var uploadTask = storageRef.put(image);
+    } else {
+        var uploadTask = storageRef.put(image);
+        var storageRef = firebase.storage().ref("images/all/" + imageName);
+        var uploadTask = storageRef.put(image);
+    }
+
+    // Option to add later: fixing unintended overwrite when using 2 of the same image names
+
+    uploadTask.on("state_changed", function(snapshot) {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("upload is " + progress + " done");
+    }, function(error) {
+        console.log(error.message);
+    }, function() {
+        storageRef.getDownloadURL().then(function(downloadURL) {
+            var postKey = firebase.database().ref(selectedVal + "/" + rawImageName).key;
+            var updates = {};
+            var postData = {
+                url: downloadURL,
+                name: imageName
+            };
+
+            // Check if image already exists in all folder
+            var ref = firebase.database().ref()
+            ref.child("all").once("value", gotUserData);
+
+            function gotUserData(snapshot) {
+                if (!snapshot.exists()) {
+                    updates["/" + selectedVal + "/" + postKey] = postData;
+                    firebase.database().ref().update(updates)
+                }
+                snapshot.forEach(userSnapshot => {
+                    console.log("goes here")
+                    if (rawImageName === userSnapshot.key) {
+                        console.log("goes here")
+                        updates["/" + selectedVal + "/" + postKey] = postData;
+                        firebase.database().ref().update(updates)
+                    } else {
+                        console.log("goes here")
+                        updates["/all/" + postKey] = postData;
+                        firebase.database().ref().update(updates)
+                        updates["/" + selectedVal + "/" + postKey] = postData;
+                        firebase.database().ref().update(updates)
+                    }
+                })
+            }
+        });
+    });
+}
+
+function retrieve(clothing) {
+    return new Promise(function(resolve, reject) {
+        try {
+            var db = firebase.database().ref().child(clothing);
+            db.once("value", pics => {
+                if (!pics.exists()) {
+                    resolve([])
+                } else {
+                    var picHtml = []
+                    pics.forEach(pic => {
+                        picHtml.push(pic.val().url)
+                    })
+                    resolve(picHtml)
+                }
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
 }
